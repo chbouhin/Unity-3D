839b24f0a2bd22b2c07123f01fb828841379a436
yosmont <yann.osmont@epitech.eu>
2021-11-12 00:44:33 +0100

    Use new flashlight & Gun assets

--- Assets/Scripts/Entities/Player/Flashlight.cs
+++ Assets/Scripts/Entities/Player/Flashlight.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    public GameObject _light;
+    //public GameObject _light;
     public float _batteryCapacity = 5f;
     public float _batteryReloadFactor = 4f;
+    [Range(0, 1)] public float _minIntensity = 0.4f;
     private float _batteryTimer;
+    private Flashlight_PRO _flashlightProManager;
+    private bool _isActive = false;
 
     private void Awake()
     {
         _batteryTimer = _batteryCapacity;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _light.SetActive(false);
+        _flashlightProManager = gameObject.GetComponent<Flashlight_PRO>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_batteryTimer != 0f && Input.GetKeyDown("f"))
-            _light.SetActive(!_light.activeInHierarchy);
-        if (_light.activeInHierarchy) {
+        if (_batteryTimer != 0f && Input.GetKeyDown("f")) {
+            _flashlightProManager.Switch();
+            _isActive = !_isActive;
+        }
+        if (_isActive) {
             _batteryTimer -= Time.deltaTime;
-            if (_batteryTimer <= 0f)
-                _light.SetActive(false);
+            _flashlightProManager.Change_Intensivity((_minIntensity + ((_batteryTimer / _batteryCapacity) * (1 - _minIntensity))) * 100);
+            if (_batteryTimer <= 0f) {
+                _flashlightProManager.Switch();
+                _isActive = false;
+            }
         } else if (_batteryTimer <= _batteryCapacity)
             _batteryTimer += Time.deltaTime / _batteryReloadFactor;
     }
