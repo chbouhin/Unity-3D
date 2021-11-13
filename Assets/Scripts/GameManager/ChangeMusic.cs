c354486804045e3c13f041426db08ff7c3732bc1
yosmont <yann.osmont@epitech.eu>
2021-11-13 17:28:48 +0100

    use [SerializeField]

--- Assets/Scripts/GameManager/ChangeMusic.cs
+++ Assets/Scripts/GameManager/ChangeMusic.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ChangeMusic : MonoBehaviour
 {
-    public string _theme = null;
-    public string _regroupement = null;
+    [SerializeField] private string _theme = null;
+    [SerializeField] private string _regroupement = null;
 
     // Start is called before the first frame update
     void Start()
