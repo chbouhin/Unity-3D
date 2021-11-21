6236e0b70374f109814e566cbeee7ec43fb1dafb
chbouhin <christophe.bouhin@epitech.eu>
2021-11-21 16:51:11 +0000

    Add ogre mob manager basic system

---
+++ Assets/Scripts/Entities/Mobs/Manager/OgreMobManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OgreMobManager : AMobManager
+{
+    private bool _isRunning = false;
+
+    protected override void AnimIdleWait()
+    {
+    }
+
+    protected override void AnimIdleAttack()
+    {
+    }
+
+    protected override void AnimAttacking()
+    {
+        _animator.Play("Attack" + Random.Range(1, 3).ToString());
+    }
+
+    protected override void AnimMoving(bool move)
+    {
+        if (move) {
+            if (!_isRunning)
+                _animator.SetInteger("IsMoving", 2);
+            else
+                _animator.SetInteger("IsMoving", 1);
+        } else
+            _animator.SetInteger("IsMoving", 0);
+    }
+
+    protected override string GetIdleAttackName()
+    {
+        return "Idle";
+    }
+
+    protected override string GetMoveName()
+    {
+        if (!_isRunning)
+            return "Run";
+        else
+            return "Walk";
+    }
+}
