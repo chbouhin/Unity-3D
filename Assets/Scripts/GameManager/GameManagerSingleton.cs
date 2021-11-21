08fbd3bad46b1b3989fd4585a3d8f53c316a7a21
yosmont <yann.osmont@epitech.eu>
2021-11-21 06:15:53 +0100

    Add score, inGameTimer and leaderboard to GameManager

--- Assets/Scripts/GameManager/GameManagerSingleton.cs
+++ Assets/Scripts/GameManager/GameManagerSingleton.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,23 @@ using UnityEngine;
 public class GameManagerSingleton : MonoBehaviour
 {
     public static GameManagerSingleton _instance;
+    #region Music-variable
     private Dictionary<string, Dictionary<string, List<AudioClip>>> _musicCollection;
     private string _currentRegroupement = "All";
     private string _currentTheme = "Menu";
     private AudioSource _audioPlayer;
     private bool _isPlayingMusic = false;
+    #endregion
+    #region InGameTime-variable
+    private float _inGameTimer = 0f;
+    private bool _isTimerActive = false;
+    #endregion
+    #region Score-variable
+    private int _currentScore;
+    #endregion
+    #region Leaderboard-variable
+    private string _saveFilePath;
+    #endregion
 
     private void Awake()
     {
@@ -21,6 +35,7 @@ public class GameManagerSingleton : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             LoadAllMusic();
             _audioPlayer = gameObject.GetComponent<AudioSource>();
+            _saveFilePath = Application.persistentDataPath + "/leaderboard.save";
         }
     }
 
@@ -28,8 +43,11 @@ public class GameManagerSingleton : MonoBehaviour
     {
         if (_isPlayingMusic && !_audioPlayer.isPlaying)
             PlayMusic();
+        if (_isTimerActive)
+            _inGameTimer += Time.deltaTime;
     }
 
+    #region Music
     public void PlayMusic(string theme = null, string regroupement = null)
     {
         _currentTheme = (theme == null || theme == "") ? _currentTheme : theme;
@@ -82,4 +100,102 @@ public class GameManagerSingleton : MonoBehaviour
             }
         }
     }
+    #endregion
+    #region InGameTime
+    public float GetInGameTime()
+    {
+        return _inGameTimer;
+    }
+
+    public void StartTimer()
+    {
+        _isTimerActive = true;
+    }
+
+    public void StopTimer()
+    {
+        _isTimerActive = false;
+    }
+
+    public void ResetTimer()
+    {
+        _inGameTimer = 0f;
+    }
+    #endregion
+    #region Score
+    public int GetCurrentScore()
+    {
+        return _currentScore;
+    }
+
+    public void AddToScore(int toAdd)
+    {
+        _currentScore += toAdd;
+    }
+    #endregion
+    #region Leaderboard
+    public List<SavedScoreData> LoadLeaderboard()
+    {
+        if (File.Exists(_saveFilePath)) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(_saveFilePath, FileMode.Open);
+            List<SavedScoreData> leaderboard = (List<SavedScoreData>)bf.Deserialize(file);
+            file.Close();
+            return leaderboard;
+        } else
+            return null;
+    }
+
+    public void SaveScore()
+    {
+        List<SavedScoreData> leaderboard = LoadLeaderboard();
+        if (leaderboard != null)
+            File.Delete(_saveFilePath);
+        else
+            leaderboard = new List<SavedScoreData>();
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream saveFile = File.Create(_saveFilePath);
+        leaderboard.Add(new SavedScoreData {
+            inGameTime = _inGameTimer,
+            score = _currentScore
+        });
+        bf.Serialize(saveFile, leaderboard);
+        saveFile.Close();
+    }
+
+    public List<SavedScoreData> GetBestScore(int size = 10)
+    {
+        List<SavedScoreData> leaderboard = LoadLeaderboard();
+        leaderboard.Sort(delegate(SavedScoreData a, SavedScoreData b) {
+            if (a.score > b.score) return 1;
+            else if (a.score < b.score) return -1;
+            else return 0;
+        });
+        List<SavedScoreData> ret = new List<SavedScoreData>();
+        for (int i = 0; ret.Count != size && leaderboard.Count < i; ++i)
+            ret.Add(leaderboard[i]);
+        return ret;
+    }
+
+    public List<SavedScoreData> GetBestTime(int size = 10)
+    {
+        List<SavedScoreData> leaderboard = LoadLeaderboard();
+        leaderboard.Sort(delegate (SavedScoreData a, SavedScoreData b) {
+            if (a.inGameTime > b.inGameTime) return -1;
+            else if (a.inGameTime < b.inGameTime) return 1;
+            else return 0;
+        });
+        List<SavedScoreData> ret = new List<SavedScoreData>();
+        for (int i = 0; ret.Count != size && leaderboard.Count < i; ++i)
+            ret.Add(leaderboard[i]);
+        return ret;
+    }
+    #endregion
+}
+
+[System.Serializable]
+public class SavedScoreData
+{
+    public int score;
+    public float inGameTime;
 }
