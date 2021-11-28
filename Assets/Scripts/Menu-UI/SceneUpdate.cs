0f62dd32b33629434c1daae69028043b3d531842
chbouhin <christophe.bouhin@epitech.eu>
2021-11-28 15:05:52 +0000

    Add prefab in scene 2 and debug music menu

--- Assets/Scripts/Menu-UI/SceneUpdate.cs
+++ Assets/Scripts/Menu-UI/SceneUpdate.cs
@@ -7,6 +7,7 @@ public class SceneUpdate : MonoBehaviour
 {
     public void LoadScene(string scene)
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(scene);
     }
 
@@ -25,6 +26,7 @@ public class SceneUpdate : MonoBehaviour
 
     public void ReloadScene()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
